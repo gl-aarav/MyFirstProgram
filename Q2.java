cfeb6977a8325c7cfda50e401f164cdf5b0a839d
gl-aarav <152468456+gl-aarav@users.noreply.github.com>
2024-09-07 20:58:34 +0000

    Initial commit

---
+++ Q2.java
@@ -0,0 +1,9 @@
+public class Q2{
+  public static void main (String[] args){
+    System.out.println("Name \t Grade");
+    System.out.println("John \t 82");
+    System.out.println("Paul \t 91");
+    System.out.println("George \t 98");
+    System.out.println("Ringo \t 79");
+  }
+}
---
+++ Q3.java
@@ -0,0 +1,12 @@
+public class Q3{
+  public static void main (String [] args){
+    System.out.println("\n\n\n");
+    System.out.println("\t\t\t   *");
+    System.out.println("\t\t\t   *");
+    System.out.println("\t\t\t   *");
+    System.out.println("\t\t\t   *");
+    System.out.println("\t\t\t   *");
+    System.out.println("\t\t\t   *");
+    System.out.println("\t   * * * * * * * *");
+  }
+}
